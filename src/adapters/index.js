b87edc5c05a2dd60922bf06c767d6c3c71927c92
Georgios Karametas <geometas@hotmail.com>
2019-10-12 21:31:27 +0100

    Minor improvements

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,9 +1,4 @@
 import httpAdapter from './httpAdapter';
 import testAdapter from './testAdapter';
 
-let adapter;
-
-if (process.env.NODE_ENV !== 'production') adapter = testAdapter;
-if (process.env.NODE_ENV === 'production') adapter = httpAdapter;
-
-export default adapter;
+export default process.env.NODE_ENV === 'production' ? httpAdapter : testAdapter;
