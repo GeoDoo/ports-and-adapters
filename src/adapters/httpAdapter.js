1d623cc27f434b8beb24bef0873323cf40c0d9ee
Georgios Karametas <geometas@hotmail.com>
2019-10-12 22:26:18 +0100

    Extract base api url

--- src/adapters/httpAdapter.js
+++ src/adapters/httpAdapter.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const baseUrl = 'https://geodoo.work/wp-json/wp/v2';
+
 export default {
-    getBlogPosts: () => axios.get('https://geodoo.work/wp-json/wp/v2/posts')
+    getBlogPosts: () => axios.get(`${baseUrl}/posts`)
 };
