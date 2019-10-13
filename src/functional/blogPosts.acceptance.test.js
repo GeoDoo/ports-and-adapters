21aba8aa025318d87bab7359166b054d945f7499
Georgios Karametas <geometas@hotmail.com>
2019-10-13 13:25:31 +0100

    A more business like acceptance test

--- src/functional/blogPosts.acceptance.test.js
+++ src/functional/blogPosts.acceptance.test.js
@@ -2,5 +2,14 @@ Feature('Blog posts list');
 
 Scenario('I can see the blog posts list', (I) => {
     I.amOnPage('http://localhost:3000');
-    I.seeElement('[data-testid="blog-posts"]');  
+    I.see('We are the champions');
+    I.see('Johnson at the EU');
+    I.see('Hey three');
+    I.see('Hohoho four');
+    I.see('Now now now');
+    I.see('This is it!');
+    I.see('Problems');
+    I.see('But why?');
+    I.see('Here it is Sir!');
+    I.see('Bad ass motherfuckers');
 });
