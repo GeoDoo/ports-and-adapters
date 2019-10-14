d3f70c4fb567ab627ca24d087d10afd5bc89f2bc
Georgios Karametas <geometas@hotmail.com>
2019-10-14 22:30:34 +0100

    Fix acceptance test

--- src/functional/blogPosts.acceptance.test.js
+++ src/functional/blogPosts.acceptance.test.js
@@ -2,5 +2,5 @@ Feature('Blog');
 
 Scenario('I can see a list of blog posts when I go to the homepage', (I) => {
     I.amOnPage('http://localhost:3000');
-    I.seeNumberOfElements('.blog-post', 10);
+    I.seeNumberOfElements('[data-testid="blog-post"]', 10);
 });
