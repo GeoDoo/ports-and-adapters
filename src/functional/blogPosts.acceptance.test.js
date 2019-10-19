e842e0a2478fdd0cef0516ac7a82fab9c99e6438
Georgios Karametas <geometas@hotmail.com>
2019-10-19 19:02:59 +0100

    Minor change

--- src/functional/blogPosts.acceptance.test.js
+++ src/functional/blogPosts.acceptance.test.js
@@ -1,6 +1,6 @@
 Feature('Blog');
 
-Scenario('I can see a list of blog posts when I go to the homepage', (I) => {
+Scenario('I can see a list of 10 blog posts when I go to the homepage', (I) => {
     I.amOnPage('http://localhost:3000');
     I.seeNumberOfElements('[data-testid="blog-post"]', 10);
 });
